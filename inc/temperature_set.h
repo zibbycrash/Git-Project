3a6538abf3cbb57deeadc2fdc8439d725474bff8
Zuber Ahmed <40126210+zibbycrash@users.noreply.github.com>
2021-07-28 00:34:31 +0530

    Update temperature_set.h

--- inc/temperature_set.h
+++ inc/temperature_set.h
@@ -1,9 +1,9 @@
 /**
  * @file temperature_set.h
- * @author Gobikumaar Sivagnanam
+ * @author Zuber Ahmed
  * @brief Function to read the input from ADC and store this
  * @version 0.1
- * @date 2021-04-29
+ * @date 2021-07-28
  * 
  * @copyright Copyright (c) 2021
  * 
